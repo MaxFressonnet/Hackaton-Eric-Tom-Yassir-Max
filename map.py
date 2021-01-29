c60dd82d987bf349ea0a031b41ac00d5aca4f93b
MaxFressonnet <max.fressonnet@mines-paristech.fr>
2021-01-29 10:55:52 +0100

    Classe Map

---
+++ map.py
@@ -0,0 +1,28 @@
+
+
+
+class Map:
+
+    def __init__(self,mur,porte,couloir):
+        self.murs = []
+        for k in range(len(mur)):
+            for i in range(mur[k][0][0],mur[k][1][0]):
+                self.murs.append([mur[k][0][1],i])
+                self.murs.append([mur[k][1][1],i])
+            for j in range(mur[k][0][1],mur[k][1][1]):
+                self.murs.append(j,mur[k][0][0])
+                self.murs.append(j,mur[k][1][0])
+        self.portes = porte
+        for i in range(len(self.murs)):
+            for y in porte:
+                if self.murs[i]==y:
+                    del(self.murs[i])
+        self.couloirs = []
+        for k in range(len(couloir)):
+            for i in range(len(couloir[k])-1):
+                if couloir[k][i][0] != couloir[k][i + 1][0]:
+                    for j in range(couloir[k][i][0],couloir[k][i + 1][0]):
+                        self.couloirs.append(j,couloir[k][i][1])
+                else:
+                    for m in range(couloir[k][i][1],couloir[k][i + 1][1]):
+                        self.couloirs.append(couloir[k][i][0],m)
